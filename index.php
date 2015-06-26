84d6a071d864d79dd1b93af5252e7c56c0095815
sisadmin2 <sisadmin2@sisadmin-2.tabakvintorg.gml.local>
2015-06-26 13:31:53 +0300

    * Упорядочил файлы; * Добавил картинки (вкл/отк музыка, завод );

--- index.php
+++ index.php
@@ -32,16 +32,17 @@
                 clear: both;
             }
         </style>
-        <script type="text/javascript" src="jquery.js"></script>
-        <script type="text/javascript" src="buzz.min.js"></script>
-        <script type="text/javascript" src="jquery.keyboard.js"></script>
-        <script src="pixi.js"></script>   
-        <script src="box2d.min.js"></script>
+        <script type="text/javascript" src="js/libs/jquery.js"></script>
+        <script type="text/javascript" src="js/libs/buzz.min.js"></script>
+        <script type="text/javascript" src="js/libs/jquery.keyboard.js"></script>
+        <script src="js/libs/pixi.js"></script>   
+        <script src="js/libs/box2d.min.js"></script>
 
         <script src="classes/fons.js"></script>
         <script src="classes/player.js"></script>
 
-        <script src="objects/level_1.js"></script>
+        
+        <script src="js/levels/level_1.js"></script>
 
 
         <script src="myminigame.js"></script>
@@ -49,20 +50,6 @@
     </head>
 
     <body>
-        <!--        <div class="comments">
-                     Put this script tag to the <head> of your page 
-                    <script type="text/javascript" src="//vk.com/js/api/openapi.js?116"></script>
-        
-                    <script type="text/javascript">
-                        VK.init({apiId: 4736921, onlyWidgets: true});
-                    </script>
-        
-                     Put this div tag to the place, where the Comments block will be 
-                    <div id="vk_comments"></div>
-                    <script type="text/javascript">
-                        VK.Widgets.Comments("vk_comments", {limit: 5, width: "400", attach: "*"});
-                    </script>
-                </div>-->
         <div class="conteiner">
             <div  class="game">
             </div>
@@ -79,10 +66,7 @@
         </div>
         <div class="clear"></div>
 
-
         <a target="_blank" href="http://www.youtube.com/id22066662">Ссылка на канал автора=)</a>
-
-
 
     </body>
 </html>
